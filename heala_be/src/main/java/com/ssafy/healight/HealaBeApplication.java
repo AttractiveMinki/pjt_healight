6ce8f58be4ad91fe9e5e8127d2b82a2fc3c9cd75
sachieve7 <sachieve7@naver.com>
2021-07-25 18:21:10 +0900

    Beckend Initial commit

---
+++ heala_be/src/main/java/com/ssafy/healight/HealaBeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.healight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealaBeApplication.class, args);
+	}
+
+}
