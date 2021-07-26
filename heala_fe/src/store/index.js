504e96bab48c94ce05c8239f51f0b5e9627c04a4
AttractiveMinki <wnduq125@naver.com>
2021-07-26 14:06:29 +0900

    feat: synchronize branch

--- heala_fe/src/store/index.js
+++ heala_fe/src/store/index.js
@@ -1,11 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import SERVER from '@/api/drf.js'
+import router from '@/router/index.js'
+import axios from 'axios'
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    username: 'annonymous_User',
+  },
   mutations: {},
-  actions: {},
+  actions: {
+    join: function (context, data) {
+      axios({
+        url: SERVER.URL + SERVER.ROUTES.signup,
+        method: 'post',
+        data: data,
+      })
+      .then(() => {
+        router.push({ name: 'Login' })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+  },
   modules: {},
 });
