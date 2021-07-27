6eea3206753a0b1c12c386d70c1c796542d3f64b
류한길 <qer7391@naver.com>
2021-07-27 20:34:58 +0900

    Feat: [Frontend] fontawesome 사용

--- heala_fe/src/main.js
+++ heala_fe/src/main.js
@@ -7,7 +7,8 @@ import Element from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "./plugins/element.js";
 
-import '@/assets/css/main.css'
+import "@/assets/css/main.css";
+import "@/fontAwesomeIcon.js";
 
 Vue.config.productionTip = false;
 Vue.use(Element);
