d4304ad0e6fa0d6f1f946874ad7a6039e02e932e
annamon <annamon20@likelion.org>
2021-07-27 12:08:36 +0900

    Fix: [Profile] 프로필 정보 불러오기 수정 #S05P12A605-117

--- heala_be/src/main/java/com/ssafy/healight/controller/UserController.java
+++ heala_be/src/main/java/com/ssafy/healight/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.ssafy.healight.controller;
 
-import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.healight.domain.entity.User;
+import com.ssafy.healight.domain.repository.UserRepository;
+
+import io.swagger.annotations.ApiOperation;
 
 
 @CrossOrigin("*")
@@ -19,52 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 	
-	/*
 	@Autowired
-	private UserService userService;
-
-	// 회원 가입
-	@PostMapping("/signup")
-	public boolean signUp(@RequestBody User user) {
-		return userService.signUp(user);
-	}
-
-	// 회원 정보 불러오기
-	@GetMapping("/{id}")
-	public User getUser(@PathVariable String id) {
-		return userService.getUser(id);
-	}
-
-	// 회원 정보 수정
-	@PutMapping("/modify")
-	public boolean modifyUser(@RequestBody User user) {
-		return userService.modifyUser(user);
-	}
-
-	// 회원 탈퇴
-	@PutMapping("/{id}")
-	public boolean deleteUser(@RequestBody User user) {
-		return userService.deleteUser(user);
-	}
-
-	// 로그인
-	@PostMapping("/login")
-	public String login(@RequestBody User user, HttpSession session) {
-		String name = userService.login(user.getId(), user.getPassword());
-		if (name != null) {
-			session.setAttribute("id", user.getId());
-			session.setAttribute("name", name);
-			return name;
-		} else {
-			return "false";
+    UserRepository userDao;
+	
+	@ApiOperation(value = "프로필 편집 조회하기.")
+	@GetMapping("/profile/{user_id}")
+	public Object getUser(@PathVariable int user_id) {
+		Optional<User> userOpt = Optional.of(userDao.getUserById(user_id));
+		if(userOpt.isPresent()) {
+			return new ResponseEntity<Optional<User>>(userOpt, HttpStatus.OK);
 		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
-
-	// 로그아웃
-	@GetMapping("/logout")
-	public boolean logout(HttpSession session) {
-		session.invalidate();
-		return true;
-	}
-	*/
 }
