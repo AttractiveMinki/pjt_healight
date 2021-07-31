b8087f3ccaa05548d42cac70858cb4ba95047855
sachieve7 <sachieve7@naver.com>
2021-07-31 17:46:27 +0900

    Feat: [Community] 글 목록 불러오기 기능 구현 #S05P12A605-43

--- heala_be/src/main/java/com/ssafy/healight/controller/CommunityController.java
+++ heala_be/src/main/java/com/ssafy/healight/controller/CommunityController.java
@@ -1,13 +1,34 @@
 package com.ssafy.healight.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.healight.service.CommunityService;
+
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin("*")
 @RequestMapping("/community")
 @RestController
-
 public class CommunityController {
-
+	
+	@Autowired
+	private CommunityService communityService;
+	
+	@ApiOperation(value = "커뮤니티 전체 글 목록 가져오기")
+	@GetMapping
+	public Object getAllPostList() {
+		return communityService.getAllPostList();
+	} 
+	
+	@ApiOperation(value = "카테고리와 서브 카테고리에 맞는 글 목록 가져오기")
+	@GetMapping("/category")
+	public Object getPostList(@RequestParam(value="category") int category, @RequestParam(value="sub_category") int subCategory) {
+		return communityService.getPostList(category, subCategory);
+	}
+	
 }
