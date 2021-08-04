4e556e1a12a06a6932ece11419f6ddb225322edd
AttractiveMinki <wnduq125@naver.com>
2021-08-04 13:20:18 +0900

    Feat: [Profile] 더보기 버튼 업데이트 #S05P13A605-97

--- heala_fe/src/components/my_page/Navbar.vue
+++ heala_fe/src/components/my_page/Navbar.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
-    <div class="display-flex justify-content-space-between align-items">
+    <!-- <div class="display-flex justify-content-space-between align-items">
       <span class="display-flex"><font-awesome-icon icon="arrow-left" class="padding-left cursor-pointer" style="margin: 2vw" @click="goBack()"/><span class="text-title">{{ username }}spartakim</span></span>
       
-      <!-- <button class="button-color">작성</button> -->
       <span style="margin: 2vw">
         <font-awesome-icon icon="ellipsis-v" style="margin: 2vw" />
         <font-awesome-icon :icon="['far', 'bell']" style="margin: 2vw" />
       </span>
-    </div>
+    </div> -->
+    <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+      <el-row class="display-flex justify-content-space-between align-items">
+        <el-col :span="18">
+          <el-menu-item index="1" class="display-flex"><span class="display-flex align-items"><font-awesome-icon icon="arrow-left" class="padding-left cursor-pointer" style="margin: 2vw" @click="goBack()"/></span><span class="text-title padding-left">{{ username }}spartakim</span></el-menu-item>
+        </el-col>
+        <el-col :span="6" class="padding-right">
+          <el-submenu index="2">
+            <template slot="title"><font-awesome-icon icon="ellipsis-v" /></template>
+              <el-menu-item index="2-1">
+                <router-link :to="{ name: 'Scrap' }" class="text-decoration-none">저장한 게시글</router-link>
+              </el-menu-item>
+            <el-menu-item index="2-2">
+              <router-link :to="{ name: 'ChangePassword' }" class="text-decoration-none">비밀번호 변경</router-link>
+            </el-menu-item>
+            <el-menu-item index="2-3" @click="logout">로그아웃</el-menu-item>
+            <!-- <el-menu-item index="2-4">신고</el-menu-item> -->
+          </el-submenu>
+          <!-- <el-menu-item index="3">
+            <font-awesome-icon :icon="['far', 'bell']" style="margin: 2vw" />
+          </el-menu-item> -->
+        </el-col>
+      </el-row>
+      
+
+    </el-menu>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex"
+import { mapActions, mapState } from "vuex"
 
 export default {
   name: "Navbar",
@@ -21,6 +45,9 @@ export default {
     goBack: function () {
       this.$router.go(-1)
     },
+    ...mapActions([
+      'logout',
+    ]),
   },
   computed: {
     ...mapState([
@@ -43,6 +70,9 @@ export default {
   .padding-left {
     padding-left: 4%;
   }
+  .padding-right {
+    padding-right: 4%;
+  }
   .cursor-pointer {
     cursor: pointer;
   }
@@ -50,5 +80,9 @@ export default {
     font-weight: bold; 
     font-size: 25px;
     margin-bottom: 5px;
+  }
+  .text-decoration-none {
+    text-decoration: none;
+    color: #899399;
   }
 </style>
