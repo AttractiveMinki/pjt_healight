8cb5022988317cfbdfacacd5083aa77334165bbe
AttractiveMinki <wnduq125@naver.com>
2021-08-05 10:42:12 +0900

    Fix: [Merge] index3.js, Navbar.vue 충돌 수정

--- heala_fe/src/router/index3.js
+++ heala_fe/src/router/index3.js
@@ -11,7 +11,6 @@ import WithDetailCertify from "@/views/challenge/with/WithDetailCertify"
 import WithDetailReview from "@/views/challenge/with/WithDetailReview"
 import WithMainDiet from "@/views/challenge/with/WithMainDiet"
 import WithMainHeart from "@/views/challenge/with/WithMainHeart"
-<<<<<<< HEAD
 import Profile from "@/views/my_page/Profile";
 import ProfileTodoList from "@/views/my_page/ProfileTodoList";
 import ProfilePhysInfo from "@/views/my_page/ProfilePhysInfo";
@@ -20,6 +19,12 @@ import ChangePassword from "@/views/my_page/showmore/ChangePassword";
 import DietRecord from "@/views/my_page/record/DietRecord";
 import DietRecordMake from "@/views/my_page/record/DietRecordMake";
 import WeightCalender from "@/views/my_page/record/WeightCalender";
+import KiwiMain from "@/views/challenge/kiwi/KiwiMain"
+import KiwiHealth from "@/views/challenge/kiwi/KiwiHealth"
+import KiwiDiet from "@/views/challenge/kiwi/KiwiDiet"
+import KiwiHeart from "@/views/challenge/kiwi/KiwiHeart"
+import KiwiSpecial from "@/views/challenge/kiwi/KiwiSpecial"
+import MyMain from "@/views/challenge/my/MyMain"
 
 export default [
   {
@@ -127,109 +132,99 @@ export default [
     name: "WeightCalender",
     component: WeightCalender,
   },
-=======
-import KiwiMain from "@/views/challenge/kiwi/KiwiMain"
-import KiwiHealth from "@/views/challenge/kiwi/KiwiHealth"
-import KiwiDiet from "@/views/challenge/kiwi/KiwiDiet"
-import KiwiHeart from "@/views/challenge/kiwi/KiwiHeart"
-import KiwiSpecial from "@/views/challenge/kiwi/KiwiSpecial"
-import MyMain from "@/views/challenge/my/MyMain"
-
-export default [
-    {
-        path: "/user/signup",
-        name: "Signup",
-        component: Signup,
-      },
-      {
-        path: "/user/login",
-        name: "Login",
-        component: Login,
-      },
-      {
-        path: "/community/Communitymain",
-        name: "CommunityMain",
-        component: CommunityMain,
-      },
-      {
-        path: "/community/health",
-        name: "Health",
-        component: Health,
-      },
-      {
-        path: "/community/diet",
-        name: "Diet",
-        component: Diet,
-      },
-      {
-        path: "/community/heart",
-        name: "Heart",
-        component: Heart,
-      },
-      {
-        path: "/challenge/with/withmain",
-        name: "WithMain",
-        component: WithMain,
-      },
-      {
-        path: "/challenge/with/withmake",
-        name: "WithMake",
-        component: WithMake,
-      },
-      {
-        path: "/challenge/with/withmaindiet",
-        name: "WithMainDiet",
-        component: WithMainDiet,
-      },
-      {
-        path: "/challenge/with/withmainheart",
-        name: "WithMainHeart",
-        component: WithMainHeart,
-      },            
-      {
-        path: "/challenge/with/:id",
-        name: "WithDetail",
-        component: WithDetail,
-      },
-      {
-        path: "/challenge/with/:id/certify",
-        name: "WithDetailCertify",
-        component: WithDetailCertify,
-      },
-      {
-        path: "/challenge/with/:id/review",
-        name: "WithDetailReview",
-        component: WithDetailReview,
-      },
-      {
-        path: "/challenge/kiwimain",
-        name: "KiwiMain",
-        component: KiwiMain,
-      },
-      {
-        path: "/challenge/kiwihealth",
-        name: "KiwiHealth",
-        component: KiwiHealth,
-      },
-      {
-        path: "/challenge/kiwidiet",
-        name: "KiwiDiet",
-        component: KiwiDiet,
-      },
-      {
-        path: "/challenge/kiwiheart",
-        name: "KiwiHeart",
-        component: KiwiHeart,
-      },
-      {
-        path: "/challenge/kiwispecial",
-        name: "KiwiSpecial",
-        component: KiwiSpecial,
-      },
-      {
-        path: "/challenge/my/mymain",
-        name: "MyMain",
-        component: MyMain,
-      },
->>>>>>> 1605187157d1376bbee3928575529ec2d4c536b8
+  {
+    path: "/user/signup",
+    name: "Signup",
+    component: Signup,
+  },
+  {
+    path: "/user/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/community/Communitymain",
+    name: "CommunityMain",
+    component: CommunityMain,
+  },
+  {
+    path: "/community/health",
+    name: "Health",
+    component: Health,
+  },
+  {
+    path: "/community/diet",
+    name: "Diet",
+    component: Diet,
+  },
+  {
+    path: "/community/heart",
+    name: "Heart",
+    component: Heart,
+  },
+  {
+    path: "/challenge/with/withmain",
+    name: "WithMain",
+    component: WithMain,
+  },
+  {
+    path: "/challenge/with/withmake",
+    name: "WithMake",
+    component: WithMake,
+  },
+  {
+    path: "/challenge/with/withmaindiet",
+    name: "WithMainDiet",
+    component: WithMainDiet,
+  },
+  {
+    path: "/challenge/with/withmainheart",
+    name: "WithMainHeart",
+    component: WithMainHeart,
+  },            
+  {
+    path: "/challenge/with/:id",
+    name: "WithDetail",
+    component: WithDetail,
+  },
+  {
+    path: "/challenge/with/:id/certify",
+    name: "WithDetailCertify",
+    component: WithDetailCertify,
+  },
+  {
+    path: "/challenge/with/:id/review",
+    name: "WithDetailReview",
+    component: WithDetailReview,
+  },
+  {
+    path: "/challenge/kiwimain",
+    name: "KiwiMain",
+    component: KiwiMain,
+  },
+  {
+    path: "/challenge/kiwihealth",
+    name: "KiwiHealth",
+    component: KiwiHealth,
+  },
+  {
+    path: "/challenge/kiwidiet",
+    name: "KiwiDiet",
+    component: KiwiDiet,
+  },
+  {
+    path: "/challenge/kiwiheart",
+    name: "KiwiHeart",
+    component: KiwiHeart,
+  },
+  {
+    path: "/challenge/kiwispecial",
+    name: "KiwiSpecial",
+    component: KiwiSpecial,
+  },
+  {
+    path: "/challenge/my/mymain",
+    name: "MyMain",
+    component: MyMain,
+  },
 ]
