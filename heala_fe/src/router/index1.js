b9ba1d94309c64dd481f1a80ce8ab3c49efad332
류한길 <qer7391@naver.com>
2021-08-11 16:40:46 +0900

    Fix: [Merge] 충돌 해결

--- heala_fe/src/router/index1.js
+++ heala_fe/src/router/index1.js
@@ -1,11 +1,8 @@
 import PostDetail from "@/views/community/PostDetail";
 import FollowExample from "@/views/FollowExample";
-<<<<<<< HEAD
 import HomeFeed from "@/views/home/HomeFeed";
-=======
 import CertifyImage from "@/views/challenge/my/CertifyImage";
 import Result from "@/views/challenge/my/Result";
->>>>>>> 4440f5d2555547c89cdaee556fd246123af9a598
 
 export default [
     {
