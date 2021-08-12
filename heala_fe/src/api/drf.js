e6e525e147463a562adc0c0f848af0d11d0149db
류한길 <qer7391@naver.com>
2021-08-12 11:14:10 +0900

    Feat: [배포] 배포 관련 설정 변경

--- heala_fe/src/api/drf.js
+++ heala_fe/src/api/drf.js
@@ -1,5 +1,6 @@
 // const SERVER_URL = process.env.VUE_APP_SERVER_URL
-const SERVER_URL = "http://127.0.0.1:8080"
+const SERVER_URL = "http://localhost:8080/api"
+// const SERVER_URL = "http://i5a605.p.ssafy.io:8080/api"
 
 // 모듈로 사용하기 위해 export default를 해야 한다.
 export default {
