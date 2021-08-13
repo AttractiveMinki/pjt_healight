16d78772fb2f4e8a20d38fb72b3dc17f9abfdf9c
AttractiveMinki <wnduq125@naver.com>
2021-08-13 11:56:09 +0900

    fix: [Footer] Footer Icon 색깔 변경

--- heala_fe/src/components/home/Footer.vue
+++ heala_fe/src/components/home/Footer.vue
@@ -1,33 +1,31 @@
 <template>
   <div id="footer" style="display: grid; align-items: center">
     <el-row>
-      <router-link :to="{ name: 'HomeFeed' }" class="text-decoration-none" @click.native="changeRouter(0)" :class="{selected: this.$store.state.selectedRouter == 0}">
+      <router-link :to="{ name: 'HomeFeed' }" class="text-decoration-none color-black" @click.native="changeRouter(0)" :class="{selected: selectedRouter == 0}">
         <el-col :span="5">
           <font-awesome-icon icon="home"/>
         </el-col>
       </router-link>
-      <router-link :to="{ name: 'CommunityMain' }" class="text-decoration-none" @click.native="changeRouter(1)" :class="{selected: this.$store.state.selectedRouter == 1}">
+      <router-link :to="{ name: 'CommunityMain' }" class="text-decoration-none color-black" @click.native="changeRouter(1)" :class="{selected:selectedRouter == 1}">
         <el-col :span="5">
           <font-awesome-icon icon="user-friends"/>
         </el-col>
       </router-link>
-      <router-link :to="{ name: 'writing' }" class="text-decoration-none" @click.native="changeRouter(2)" :class="{selected: this.$store.state.selectedRouter == 2}">
+      <router-link :to="{ name: 'writing' }" class="text-decoration-none color-black" @click.native="changeRouter(2)" :class="{selected: selectedRouter == 2}">
         <el-col :span="4">
           <font-awesome-icon :icon="['far', 'plus-square']" />
         </el-col>
       </router-link>
-      <router-link :to="{ name: 'WithMain' }" class="text-decoration-none" :class="{selected: this.$store.state.selectedRouter == 3}" @click.native="changeRouter(3)" >
+      <router-link :to="{ name: 'WithMain' }" class="text-decoration-none color-black" @click.native="changeRouter(3)" :class="{selected: selectedRouter == 3}" >
         <el-col :span="5">
           <font-awesome-icon :icon="['fas', 'running']" />
         </el-col>
       </router-link>
-      <span @click="changeRouter(4)">
-        <router-link :to="{ name: 'Profile' }" :v-model="selectedRouter" :class="{selected: this.$store.state.selectedRouter == 4}" class="text-decoration-none" >
-          <el-col :span="5">
-            <font-awesome-icon icon="user" />
-          </el-col>
-        </router-link>
-      </span>
+      <router-link :to="{ name: 'Profile' }" class="text-decoration-none color-black" @click.native="changeRouter(4)" :class="{selected: selectedRouter == 4}">
+        <el-col :span="5">
+          <font-awesome-icon icon="user" />
+        </el-col>
+      </router-link>
     </el-row>
   </div>
 </template>
@@ -75,9 +73,9 @@ export default {
     height: 3rem;
   }
   .selected {
-    color: #ADEC6E;
+    color: #ADEC6E !important;
   }
-  .normal {
+  .color-black {
     color: black;
   }
 </style>
