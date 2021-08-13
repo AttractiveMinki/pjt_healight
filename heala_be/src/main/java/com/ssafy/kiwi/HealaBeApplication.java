60bb86155b8e10db2470a616b6e8b268c37d8505
sachieve7 <sachieve7@naver.com>
2021-08-13 14:03:29 +0900

    [Project] 이미지 접속 경로 설정 #S05P13A605-173

--- heala_be/src/main/java/com/ssafy/kiwi/HealaBeApplication.java
+++ heala_be/src/main/java/com/ssafy/kiwi/HealaBeApplication.java
@@ -1,15 +1,22 @@
 package com.ssafy.kiwi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class HealaBeApplication {
+	
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="+
+			"classpath:application.properties,"+
+			"classpath:aws.properties,"+
+			"classpath:datasource.properties";
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealaBeApplication.class, args);
+		new SpringApplicationBuilder(HealaBeApplication.class)
+									.properties(APPLICATION_LOCATIONS)
+									.run(args);
 	}
 
 }
