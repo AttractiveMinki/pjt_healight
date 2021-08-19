fb8432a7b4dbb09fad479270bc5dcff264f57be6
sachieve7 <sachieve7@naver.com>
2021-08-19 11:46:28 +0900

    Feat: [Project]  CSS 적용 테스트

--- heala_fe/src/App.vue
+++ heala_fe/src/App.vue
@@ -28,4 +28,12 @@ html, body {
   padding: 0;
   margin: 0;
 }
+
+@font-face {
+    font-family: 'SDSamliphopangche_Outline';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/SDSamliphopangche_Outline.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
 </style>
+
