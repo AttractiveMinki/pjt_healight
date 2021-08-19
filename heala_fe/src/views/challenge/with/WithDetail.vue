f0c970d998cb678cc20844484e9f3c2670cbc824
AttractiveMinki <wnduq125@naver.com>
2021-08-20 02:06:23 +0900

    Fix: [Challenge] 함께챌린지 상세 이미지 구현, title 글씨 크기 축소

--- heala_fe/src/views/challenge/with/WithDetail.vue
+++ heala_fe/src/views/challenge/with/WithDetail.vue
@@ -26,7 +26,7 @@
       <el-col :span="24" class="text-decoration-title">
         <el-image class="margin-left-10"
           style="width: 100%; height: 20vh"
-          :src="value.withChallenge.image" 
+          :src="imageServer + value.withChallenge.image" 
           >
         </el-image>
       </el-col>
@@ -110,6 +110,7 @@ export default {
     return {
       value: [],
       dataLoaded: false,
+      imageServer: SERVER.IMAGE_URL,
     }
   },
   // props: {
@@ -173,7 +174,7 @@ export default {
     font-weight: bold;
   }
   .text-challenge-title {
-    font-size: 30px;
+    font-size: 20px;
     font-weight: bold;
   }
   .text-hashtag-detail {
